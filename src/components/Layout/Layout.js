5c36b9240db178d8302c8f768803bbea37a82d6a
Angel Lando <angel.lando@outlook.com>
2020-03-31 17:15:23 +0200

    Created BurgerBuilder container

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
 import Aux from '../../hoc/Aux';
+import './Layout.css';
 
 const Layout = (props) => (
     <Aux>
         <div>Toolbar, SideDrawer, Backdrop</div>
-        <main>
+        <main className="Content">
             {props.children}
         </main>
     </Aux>
