273b915620ee515e95fc7192ce16e196d432d433
Angel Lando <angel.lando@outlook.com>
2020-03-31 19:49:40 +0200

    Added Burger ingredients handling

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,8 +3,17 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const Burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients).map(key => {
+        return [...Array(props.ingredients[key])].map((_, index) => {
+            return <BurgerIngredient key={key + index} type={key} />
+        });
+    });
+
     return(
         <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top" />
+            {transformedIngredients}
+            <BurgerIngredient type="bread-bottom" />
         </div>
     );
 };
